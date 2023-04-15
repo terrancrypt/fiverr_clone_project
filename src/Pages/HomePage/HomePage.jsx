455b8d66ac9fc6a964218f925a57488d1599487b
Thai Pham Ngoc Tuong <thaiphamngoctuong@gmail.com>
2023-04-15 14:42:42 +0700

    commit

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './HomePage.css';
+import CarouselHomePage from "./Carousel/CarouselHomePage";
 
 const HomePage = () => {
   return (
-    <div className="homepage text-center py-32">
-    
+    <div className="homepage__carousel flex items-center justify-start">
+    <CarouselHomePage/>
     </div>
   );
 };
