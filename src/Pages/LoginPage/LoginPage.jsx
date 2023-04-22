fd64929042112b504518992a8be603ca149f3bea
Thai Pham Ngoc Tuong <thaiphamngoctuong@gmail.com>
2023-04-22 19:46:39 +0700

    update

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./loginPage.css";
 import { useDispatch } from "react-redux";
 import { userService } from "../../services/loginServices/loginService";
@@ -7,23 +7,22 @@ import { localUserService } from "../../services/loginServices/localService";
 import { setUserLogin } from "../../toolkit/userLogin";
 import { useNavigate } from "react-router-dom";
 const LoginPage = () => {
-  let dispatch = useDispatch();
-  let navigate = useNavigate();
-  const singInUser = (values) => {
-    userService
-      .postLogin(values)
-      .then((res) => {
-        message.success("Login thành công");
-        localUserService.set(res.data.content);
-        dispatch(setUserLogin(res.data.content));
-        console.log(res);
-        navigate("/");
-      })
-      .catch((err) => {
-        console.log(err);
-        message.error("Login thất bại");
-      });
-  };
+  // let dispatch = useDispatch();
+  // let navigate = useNavigate();
+  // const getValueFromInput = () => {
+  //   let infoUser = {
+  //     email: "",
+  //     password: "",
+  //   };
+  //   infoUser.email = document.querySelector("#email").value;
+  //   infoUser.password = document.querySelector("#password").value;
+  //   return infoUser;
+  // };
+  
+    const handleLogin = (values) => {
+      console.log(values);
+    };
+ 
 
   return (
     <>
@@ -31,7 +30,9 @@ const LoginPage = () => {
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className=" p-6 space-y-4 md:space-y-6 sm:p-8">
+              <LoginPageImg />
               <form className="space-y-4 md:space-y-6">
+<<<<<<< tuongBranch
                 <p className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Sign In to Fiverr
                 </p>
@@ -114,9 +115,11 @@ const LoginPage = () => {
                 <div className="login__separator">
                   <span className="separator__text">OR</span>
                 </div>
+=======
+>>>>>>> local
                 <div className="mb-6">
                   <input
-                    type="email"
+                    type="text"
                     id="email"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Email/Username"
@@ -138,33 +141,13 @@ const LoginPage = () => {
                 </div>
 
                 <button
-                  type="continute"
-                  onClick={singInUser}
+                  type="con"
+                  onClick={handleLogin}
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800   "
                 >
-                  Continute
+                  Log in
                 </button>
 
-                <div className="flex items-start mb-6">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="remember"
-                      type="checkbox"
-                      defaultValue
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                      required
-                    />
-                  </div>
-                  <label
-                    htmlFor="remember"
-                    className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    Remember me
-                  </label>
-                  <a href="/">
-                    <u>Forgot Password</u>
-                  </a>
-                </div>
                 <hr />
                 <div>
                   <p>
---
+++ src/Pages/LoginPage/LoginPageImg.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+
+export default function LoginPageImg() {
+  return (
+    <div>              <p className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+    Sign In to Fiverr
+  </p>
+  <div className="">
+    <button
+      type="button"
+      className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center  "
+    >
+      <svg
+        class="w-4 h-4 mr-2 -ml-1"
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fab"
+        data-icon="facebook-f"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 320 512"
+      >
+        <path
+          fill="currentColor"
+          d="M279.1 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.4 0 225.4 0c-73.22 0-121.1 44.38-121.1 124.7v70.62H22.89V288h81.39v224h100.2V288z"
+        ></path>
+      </svg>
+      Sign in with Facebook
+    </button>
+  </div>
+  <div>
+    <button
+      type="button"
+      className="text-black bg-white hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center  "
+    >
+      <svg
+         class="w-5 h-5 mr-6 -ml-1"
+        viewBox="0 0 18 19"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M9 7.84363V11.307H13.8438C13.6365 12.428 12.9994 13.373 12.0489 14.0064V16.2534H14.9562C16.6601 14.6951 17.641 12.4029 17.641 9.67839C17.641 9.04502 17.5854 8.43176 17.4792 7.84865H9V7.84363Z"
+          fill="#3E82F1"
+        ></path>
+        <path
+          d="M9.00001 14.861C6.65394 14.861 4.67192 13.2876 3.96406 11.1714H0.955627V13.4937C2.43709 16.4142 5.48091 18.4198 9.00001 18.4198C11.432 18.4198 13.4697 17.6206 14.9562 16.2533L12.0489 14.0064C11.245 14.5443 10.2135 14.861 9.00001 14.861Z"
+          fill="#32A753"
+        ></path>
+        <path
+          d="M3.96404 5.45605H0.955617C0.348876 6.66246 0 8.02972 0 9.47238C0 10.915 0.348876 12.2823 0.955617 13.4887L3.96404 11.1714C3.78202 10.6335 3.6809 10.0605 3.6809 9.47238C3.6809 8.88426 3.78202 8.31122 3.96404 7.77336V5.45605Z"
+          fill="#F9BB00"
+        ></path>
+        <path
+          d="M0.955627 5.45597L3.96406 7.77327C4.67192 5.65703 6.65394 4.08368 9.00001 4.08368C10.3197 4.08368 11.5079 4.53608 12.4382 5.42078L15.0219 2.85214C13.4646 1.40948 11.427 0.52478 9.00001 0.52478C5.48091 0.52478 2.43709 2.53043 0.955627 5.45597Z"
+          fill="#E74133"
+        ></path>
+      </svg>
+      Sign in with Google
+    </button>
+  </div>
+  <div>
+    <button
+      type="button"
+      className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 mr-2 mb-2"
+    >
+      <svg
+        class="w-5 h-5 mr-6 -ml-1"
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fab"
+        data-icon="apple"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 384 512"
+      >
+        <path
+          fill="currentColor"
+          d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"
+        ></path>
+      </svg>
+      Sign in with Apple
+    </button>
+  </div>
+  <div className="login__separator">
+    <span className="separator__text">OR</span>
+  </div></div>
+  )
+}
