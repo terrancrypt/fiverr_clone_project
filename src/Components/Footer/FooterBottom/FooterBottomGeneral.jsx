c49bc4a48f6a0ce4b020c2abf356a08716cf79f9
Thai Pham Ngoc Tuong <thaiphamngoctuong@gmail.com>
2023-04-24 12:38:13 +0700

    finish footer responsive

---
+++ src/Components/Footer/FooterBottom/FooterBottomGeneral.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const FooterBottomGeneral = () => {
+  return (
+    <div className="bottom__general flex text-gray-500 text-sm font-bold -ml-2">
+      <NavLink className="bottom__general-language flex items-center justify-center py-2 px-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-4 h-4"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+          />
+        </svg>
+        <span className="pl-1">English</span>
+      </NavLink>
+      <NavLink className="bottom__general-usd flex items-center py-2 px-3">US$ USD</NavLink>
+      <NavLink className="pl-2">
+        <svg
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          fill="#74767e"
+          xmlns="http://www.w3.org/2000/svg"
+          className="bottom__general-accessibility"
+        >
+          <circle
+            cx="16"
+            cy="16"
+            r="15.5"
+            fill="white"
+            stroke="currentColor"
+            class="circle-wrapper"
+          ></circle>
+          <path d="M16 11.5833C17.1506 11.5833 18.0834 10.6506 18.0834 9.49999C18.0834 8.3494 17.1506 7.41666 16 7.41666C14.8494 7.41666 13.9167 8.3494 13.9167 9.49999C13.9167 10.6506 14.8494 11.5833 16 11.5833Z"></path>
+          <path d="M23.9167 12.4167H8.08333C7.86232 12.4167 7.65036 12.5045 7.49408 12.6607C7.3378 12.817 7.25 13.029 7.25 13.25C7.25 13.471 7.3378 13.683 7.49408 13.8392C7.65036 13.9955 7.86232 14.0833 8.08333 14.0833H13.5V25.5417C13.5 25.8179 13.6097 26.0829 13.8051 26.2782C14.0004 26.4736 14.2654 26.5833 14.5417 26.5833C14.8179 26.5833 15.0829 26.4736 15.2782 26.2782C15.4736 26.0829 15.5833 25.8179 15.5833 25.5417V19.5H16.4167V25.5417C16.4167 25.8179 16.5264 26.0829 16.7218 26.2782C16.9171 26.4736 17.1821 26.5833 17.4583 26.5833C17.7346 26.5833 17.9996 26.4736 18.1949 26.2782C18.3903 26.0829 18.5 25.8179 18.5 25.5417V14.0833H23.9167C24.1377 14.0833 24.3496 13.9955 24.5059 13.8392C24.6622 13.683 24.75 13.471 24.75 13.25C24.75 13.029 24.6622 12.817 24.5059 12.6607C24.3496 12.5045 24.1377 12.4167 23.9167 12.4167Z"></path>
+        </svg>
+      </NavLink>
+    </div>
+  );
+};
+
+export default FooterBottomGeneral;
