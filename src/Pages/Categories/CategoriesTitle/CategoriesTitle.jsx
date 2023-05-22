2351f99cd377be4f4a5b60f367b4ab8cde754b3b
Nguyễn Thanh <k2nguyenngocthanh@gmail.com>
2023-05-22 17:38:02 +0700

    update categories title

--- src/Pages/Categories/CategoriesTitle/CategoriesTitle.jsx
+++ src/Pages/Categories/CategoriesTitle/CategoriesTitle.jsx
@@ -6,7 +6,7 @@ export default function CategoriesTitle() {
 <div className="categories__title">
   <div className="container mt-5 sm:mt-3 mb-5 sm:mb-3">
     <h1 className="text-2xl lg:text-3xl">Most popular in Digital Marketing</h1>
-    <div className="categories__content grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div className=" grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 sm:p-12 ">
       <div className="item flex items-center justify-center p-4 border border-gray-200 rounded-md">
         <img
           src="https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/97477f04af40de3aa1f8d6aa21f69725-1626179101614/Logo%20design_2x.png"
@@ -38,7 +38,7 @@ export default function CategoriesTitle() {
           className="w-16 h-16"
         />
         <span className="ml-2 text-sm">Architecture & Interior Design</span>
-        <svg
+        <svg 
           aria-hidden="true"
           focusable="false"
           data-prefix="fas"
@@ -103,29 +103,7 @@ export default function CategoriesTitle() {
         </svg>
       </div>
 
-      <div className="item flex items-center justify-center p-4 border border-gray-200 rounded-md">
-        <img
-          src="https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/97477f04af40de3aa1f8d6aa21f69725-1626179101623/T-Shirts%20_%20Merchandise_2x.png"
-          alt="..."
-          className="w-16 h-16"
-        />
-        <span className="ml-2 text-sm">T-Shirts & Merchandise</span>
-        <svg
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="fas"
-          data-icon="arrow-right"
-          className="svg-inline--fa fa-arrow-right ml-2 h-4 w-4"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-        >
-          <path
-            fill="currentColor"
-            d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
-          ></path>
-        </svg>
-      </div>
+  
     </div>
   </div>
 </div>
